0afdbf91af00bb26fdddc2f7c857c2c8dfe7a58c
swati <swatip7878@gmail.com>
2023-03-12 23:14:59 +0530

    test commit

--- src/test/java/org/test/utilities/ExcelFileHandling.java
+++ src/test/java/org/test/utilities/ExcelFileHandling.java
@@ -1,6 +1,7 @@
 package org.test.utilities;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -8,13 +9,22 @@ import org.apache.poi.ss.usermodel.Workbook;
 import java.io.*;
 
 public class ExcelFileHandling {
-    public void readParticularCell(String SheetName, int row) throws IOException {
-       File f=new File("");
-        FileInputStream fis=new FileInputStream(f);
-        Workbook workbook= new HSSFWorkbook(fis);
-       Sheet sheet= workbook.getSheet(SheetName);
-      int allrow=sheet.getLastRowNum();
-        int rowsCount = sheet.getPhysicalNumberOfRows();
+    public void readParticularCell(String SheetName, int rowUN, int columnnum) throws IOException {
+        File f = new File("");
+        FileInputStream fis = new FileInputStream(f);
+        Workbook workbook = new HSSFWorkbook(fis);
+        Sheet sheet = workbook.getSheet(SheetName);
+        int allrow = sheet.getLastRowNum();
+        for (int i = 0; i < allrow; i++) {
+            Row row =sheet.getRow(rowUN);
+          int column= row.getPhysicalNumberOfCells();
+          for(int j=0;j<column;j++){
+              Cell cell = row.getCell(columnnum);
+              String Value = cell.getStringCellValue();
+          }
 
+
+
+        }
     }
 }
